c43df1772377db6d439ae76a71493dd48444aef7
Inga Kuznetsova <kuzn137@gmail.com>
2020-08-14 21:44:20 -0700

    Add files via upload
    Function, which choose best regression for test, is added. Functions descriptions are corrected.

--- heteroscedasticity/Glejser_test.py
+++ heteroscedasticity/Glejser_test.py
@@ -18,13 +18,14 @@ class Glejser_test(Heteroscedasticity_tests):
      '''
      def __init__(self, file_name, col_x, col_y):
          Heteroscedasticity_tests.__init__(self, file_name, col_x, col_y)
+         self.scores=[]
         
      def regression_scores(self, x, y):
          '''
-         Function computes coefficient in park test regression
+         Function computes coefficient in Glejser test regression
          
-         arg: regession income x and outcome y
-         outcome R2 and rme squared errors for regression with given x and y
+         args: regession income x and outcome y
+         returns: R2 and rme squared errors for regression with given x and y
          '''
         #x = x.values.reshape(-1,1)
          model=self.model
@@ -36,7 +37,7 @@ class Glejser_test(Heteroscedasticity_tests):
     
      def glejser_1(self):
          '''
-         Function computes coefficient in park test regression
+         Function computes coefficient in Glejser test regression
          
          arg:  none
          outcome coeffitient in regression between income feature in original regression and absolute value of residuals
@@ -45,11 +46,22 @@ class Glejser_test(Heteroscedasticity_tests):
          y=self.Y
          y_new=self.find_residuals(x, y)
          return  self.regression_scores(x, y_new)
-    
-    
      def glejser_2(self):
         '''
-         Function computes coefficient in park test regression
+         Function computes coefficient in Glejser test regression
+         
+         arg:  none
+         outcome: scores in regression between square root from incoming feature in original regression and absolute value residuals
+        '''
+        x=self.X.values.reshape(-1,1)
+        y=self.Y
+        y_new = self.find_residuals(x, y)
+        x_new=np.sqrt(abs(x))
+        return  self.regression_scores(x_new, y_new)
+    
+     def glejser_3(self):
+        '''
+         Function computes coefficient in Glejser test regression
          
          arg:  none
          outcome: scores in regression between inverse income feature in original regression and absolute value residuals
@@ -59,6 +71,21 @@ class Glejser_test(Heteroscedasticity_tests):
         y_new = self.find_residuals(x, y)
         x_new=np.reciprocal(x)
         return  self.regression_scores(x_new, y_new)
+    
+     def choose_test(self):
+        '''
+         Function choose regression for Glejser test with best R2 score
+         
+         arg:  none
+         returns: R2 score, and number of Glejser regression
+        '''
+        for i in [self.glejser_1(), self.glejser_2(), self.glejser_3()]:
+            self.scores.append(i)
+            print(self.scores)
+            arr=[i[0] for i in self.scores]
+            m=max(arr)
+        return m,  arr.index(m)+1
+         
         
-#print(Glejser_test("data_1_1.csv", 'x', 'y').glejser_1())       
+#print(Glejser_test("data_1_1.csv", 'x', 'y').choose_test())       
  
