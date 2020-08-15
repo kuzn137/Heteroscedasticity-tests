2aa2ccc8406ff3d49dff79308489e84b3c975a68
Inga Kuznetsova <kuzn137@gmail.com>
2020-08-15 07:53:08 -0700

    Add files via upload
    restructured, comments are added

---
+++ Heteroscedasticity_test_general.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 12:38:37 2020
+
+@author: kuzn137
+"""
+from sklearn import linear_model as lm
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+class Heteroscedasticity_tests():
+      """ Generic class for Heteroscedasticity tests as
+            Park and Glejser methods.
+            Both methods use linear regression for functions of tested income feature and residuals as outcome
+    
+        Attributes:
+            data file, file_name is name string
+            column col_x is for incoming features
+            column col_y is for outcome
+      """
+      def __init__(self, file_name, col_x, col_y):
+         self.df=pd.read_csv(file_name)
+         #incoming feature
+         self.X = self.df[col_x].values.reshape(-1,1)
+         #outcome
+         self.Y = self.df[col_y].values.reshape(-1,1)
+         #functions from feature for different linear regressions in Glejser test
+         self.features = [self.X, np.sqrt(abs(self.X)), np.reciprocal(abs(self.X))]
+         #residuals
+         self.y_new=self.find_residuals(self.X, self.Y)
+    
+      def find_residuals(self, x, y):
+          '''
+          function computes linear regression residuals
+          args:
+              regression incoming features: x
+              regression outcome: y
+          function returns residual
+          '''
+          model = lm.LinearRegression()
+          y = self.Y
+          model.fit(x, y)
+          y_pred=model.predict(y)
+          return abs(y_pred - y)
+      
+      def find_p_value(self, x, y):
+          '''
+          computes p value for regression between x and y
+          args: x and y, income and outcome for linear regression
+          returns: p value
+          '''
+          #other library for regression to find p value, usually I like scikit learn 
+          md=sm.regression.linear_model.OLS(x, y)
+          results = md.fit()
+          print(results.summary())
+          return results.rsquared, results.pvalues[0]
+        
+        
+         
+    
---
+++ Park_test.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 10:44:26 2020
+
+@author: kuzn137
+"""
+import numpy as np
+
+#import matplotlib.pyplot as plt
+from Heteroscedasticity_test_general import Heteroscedasticity_tests
+class Park_test(Heteroscedasticity_tests):
+     '''
+     Park_test class tests heteroscedasticity computing p value for linear regression between feature in original regression under the test and squared residuals
+
+     Attributes:
+            data file, file_name is name string
+            column col_x is for incoming features
+            column col_y is for outcome
+     '''
+     def __init__(self, file_name, col_x, col_y):
+         Heteroscedasticity_tests.__init__(self, file_name, col_x, col_y)
+        
+     def park_regression(self):
+         '''
+         Function computes p value for park test regression
+         args:  none
+         returns: p value to test slope for regression between considered feature and squared residuals. If p < 0.05 we rather have Heteroscedasticity.
+         '''
+         #consider logs as suggested
+         x = np.log(np.abs(self.X))
+         pvalue = self.find_p_value(x, np.log(np.square(self.y_new)))[1]
+         if pvalue > 0.05:
+            return "P value {} is larger than 0.05, you may not have Heteroscedasticity, check the Glejser test".format(pvalue)
+         else:
+            return "P value {} is smaller than 0.05, you may have Heteroscedasticity".format(pvalue)
+ 
+#print(Park_test("data_1_1.csv", 'x', 'y').park_regression())
+        
+   
